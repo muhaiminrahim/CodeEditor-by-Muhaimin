ec9dc6b86fa0acd296c1b7d8a0729001084574d9
Abdul Muhaimin <59179400+muhaiminrahim@users.noreply.github.com>
2021-07-21 23:21:25 +0800

    Added header component (css, js and jsx files)

---
+++ src/components/common/header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css'
+
+function Header (){
+    return(
+        <section className="header">
+            <section className="logo">Code Editor <a href="https://github.com/muhaiminrahim" target="_blank" rel="noopener noreferrer">by @muhaiminrahim</a></section>
+        </section>
+    )
+}
+
+export default Header;
